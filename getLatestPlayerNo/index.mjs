24b43a1aa72b3892d38f963b97f8e385994c9004
ottlseo <dotsi@ewhain.net>
2024-05-03 17:31:33 +0900

    Update get api to receive querystring

--- getLatestPlayerNo/index.mjs
+++ getLatestPlayerNo/index.mjs
@@ -7,18 +7,29 @@ const connection = mysql.createConnection({
   database: process.env.RDS_DATABASE_NAME 
 });
 
-const getLatestPlayerNo = async () => {
-    try {
-      // LatestPlayer의 순서 정보 리턴
-      const query = 'SELECT latestPlayerSequenceNo FROM LatestPlayer';
-      const latestPlayerNo = await executeQuery(query, []);
-      return latestPlayerNo;
-    } catch (err) {
-      console.error('Error getting latest player number:', err);
-      throw err;
+const getQueuePosition = async (id) => {
+  try {
+    // 1. id를 가진 player의 순서 정보를 playerSequenceNo 변수에 저장
+    const playerSequenceQuery = 'SELECT playerSequenceNo FROM RegistrationSequence WHERE playerId = ?';
+    const playerSequenceNo = await executeQuery(playerSequenceQuery, [id]);
+
+    // 2. LatestPlayer의 순서 정보를 currentPlayerSequenceNo 변수에 저장
+    const currentPlayerSequenceQuery = 'SELECT latestPlayerSequenceNo FROM LatestPlayer';
+    const currentPlayerSequenceNo = await executeQuery(currentPlayerSequenceQuery, []);
+    
+    if (!playerSequenceNo) { // player의 순서를 불러올 수 없는 경우
+      throw new Error(`Error finding user with ID [${id}]`);
+    } else if ( !currentPlayerSequenceNo) {
+      throw new Error(`Error getting latest player info`);
     }
-  };
-  
+    // 3. 두 값을 빼서 리턴
+    return (playerSequenceNo - currentPlayerSequenceNo);
+  } catch (err) {
+    console.error('Error getting queue position:', err);
+    throw err;
+  }
+};
+
 const executeQuery = (query, values) => {
   return new Promise((resolve, reject) => {
     connection.query(query, values, (err, result) => {
@@ -30,12 +41,22 @@ const executeQuery = (query, values) => {
     });
   });
 };
-  
+
 export const handler = async (event) => {
-    try {
-        const latestPlayerNo = await getLatestPlayerNo();
-        return { latestPlayerNo }; // e.g. { "latestPlayerNo": 3 }
-    } catch (err) {
-        return { error: err.message };
-    }
-  };
+  const { queryStringParameters = {} } = event;
+  const { id } = queryStringParameters;
+
+  // const { multiValueQueryStringParameters = {} } = event;
+  // const id = multiValueQueryStringParameters.id?.[0];
+
+  if (!id) { 
+    return { error: "Please include 'id' in query parameters" };
+  }
+
+  try {  
+    const queuePosition = await getQueuePosition(id);
+    return { queuePosition };  // e.g. { "queuePosition": 15 }
+  } catch (err) {
+    return { error: err.message };
+  }
+};
